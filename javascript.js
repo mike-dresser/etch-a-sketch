a5d0f3dd81ef38cb54343ab9cfaad4cd6631c2f0
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-01-20 12:25:51 -0500

    Basic mouseenter event working

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-alert('Hello, world!');
+const container = document.querySelector('#container');
+
+for (let i = 0; i < (16 * 16); i++) {
+    const pixel = document.createElement('div');
+    container.appendChild(pixel);
+}
+const pixels = document.querySelectorAll('#container div')
+
+for (const pixel of pixels) {
+    pixel.addEventListener('mouseenter', () => {
+        pixel.style.backgroundColor = 'pink';
+    })
+        
+
+}
