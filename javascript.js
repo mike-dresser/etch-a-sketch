3520b083a1865b2146621e634aa725bf72477bf6
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-01-21 17:53:37 -0500

    Add clear button, round container

--- javascript.js
+++ javascript.js
@@ -1,15 +1,37 @@
 const container = document.querySelector('#container');
+const sizeInput = document.querySelector('#size');
+const clear = document.querySelector('#clear');
+const DEFAULT_GRID_SIZE = 16;
+sizeInput.value = DEFAULT_GRID_SIZE;
 
-for (let i = 0; i < (16 * 16); i++) {
-    const pixel = document.createElement('div');
-    container.appendChild(pixel);
+function createGrid(squareValue) {
+    const pixelSize = (320 / squareValue);
+    for (let i = 0; i < (squareValue * squareValue); i++) {
+        const pixel = document.createElement('div');
+        pixel.style.height = `${pixelSize}px`;
+        pixel.style.width = `${pixelSize}px`;
+        container.appendChild(pixel);
+    }
 }
-const pixels = document.querySelectorAll('#container div')
 
-for (const pixel of pixels) {
-    pixel.addEventListener('mouseenter', () => {
-        pixel.style.backgroundColor = 'pink';
-    })
-        
-
+function clearGrid() {
+    const pixels = container.querySelectorAll('div');
+    for (const div of pixels) {
+        container.removeChild(div);
+    }
+    createGrid(sizeInput.value);
 }
+
+container.addEventListener('mouseover', eventTarget => {
+    eventTarget.target.style.backgroundColor = 'black';
+})
+container.addEventListener('mouseleave', eventTarget => {
+    eventTarget.target.style.backgroundColor = 'hotpink';
+}, true)
+clear.addEventListener('click', () => {
+    clearGrid();
+    clear.textContent = 'Clear'
+})
+sizeInput.addEventListener('focus', () => clear.textContent = 'Change');
+
+createGrid(DEFAULT_GRID_SIZE);
