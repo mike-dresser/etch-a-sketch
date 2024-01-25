9257aac0b5acf62376ced0ddd0f161b4f9b1ed1d
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-01-25 11:43:23 -0500

    add background transition to standard mode

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 const container = document.querySelector('#container');
 const sizeInput = document.querySelector('#size');
 const clear = document.querySelector('#clear');
-const DEFAULT_GRID_SIZE = 64;
-sizeInput.value = DEFAULT_GRID_SIZE;
+const rightControls = document.querySelector('#rightControls');
 
-function createGrid(squareValue) {
-    const pixelSize = (420 / squareValue);
-    for (let i = 0; i < (squareValue * squareValue); i++) {
+const DEFAULT_GRID_WIDTH = 64;
+sizeInput.value = DEFAULT_GRID_WIDTH;
+
+function createGrid(gridWidth) {
+    const pixelSize = (560 / gridWidth);
+    for (let i = 0; i < (gridWidth * (gridWidth * 3 / 4)); i++) {
         const pixel = document.createElement('div');
         pixel.style.height = `${pixelSize}px`;
         pixel.style.width = `${pixelSize}px`;
@@ -22,16 +24,54 @@ function clearGrid() {
     createGrid(sizeInput.value);
 }
 
+let fillStyle = 'standard';
+const rainbowFill = [];
+let rainbowCounter = 0;
+
+function createRainbow() {
+    for (i = 0; i < 64; i++) {
+        const x = Math.floor(Math.random() * 4096);
+        rainbowFill.push(x.toString(16));
+    }
+}
+
 container.addEventListener('mouseover', eventTarget => {
-    eventTarget.target.style.backgroundColor = 'black';
-})
+    if (fillStyle === 'standard') {
+        eventTarget.target.style.backgroundColor = 'black';
+    }
+    if (fillStyle === 'rainbow'){
+        eventTarget.target.style.backgroundColor = `#${rainbowFill[rainbowCounter]}`;
+        rainbowCounter >= 63 ? rainbowCounter = 0 : rainbowCounter++;
+    }   
+    if (fillStyle === 'progressive'){
+        let x = 0;
+        if (eventTarget.target.hasAttribute('opacity')) {
+            x = eventTarget.target.getAttribute('opacity');
+        }
+        if (x < 1) eventTarget.target.setAttribute('opacity', Number(x) + 0.1);
+        eventTarget.target.style.backgroundColor
+             = `rgba(0, 0, 0, ${eventTarget.target.getAttribute('opacity')})`;
+        }
+    
+} 
+
+)
 container.addEventListener('mouseleave', eventTarget => {
-    eventTarget.target.style.backgroundColor = 'rgb(33,33,33)';
 }, true)
+
 clear.addEventListener('click', () => {
     clearGrid();
     clear.textContent = 'Clear'
 })
 sizeInput.addEventListener('focus', () => clear.textContent = 'Change');
 
-createGrid(DEFAULT_GRID_SIZE);
+
+
+rightControls.addEventListener('click', eventTarget => {
+    fillStyle = eventTarget.target.id;
+})
+
+
+
+createGrid(DEFAULT_GRID_WIDTH);
+createRainbow();
