ebd0363a8d187f68eac11412ea4ea3a3dec16be6
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-01-26 13:02:48 -0500

    Working in all modes

--- javascript.js
+++ javascript.js
@@ -1,15 +1,22 @@
 const container = document.querySelector('#container');
-const sizeInput = document.querySelector('#size');
-const clear = document.querySelector('#clear');
-const rightControls = document.querySelector('#rightControls');
 
+const sizeInput = document.querySelector('#size');
 const DEFAULT_GRID_WIDTH = 64;
 sizeInput.value = DEFAULT_GRID_WIDTH;
 
+const clearButton = document.querySelector('#clear');
+const rightControls = document.querySelector('#rightControls');
+
+// Fill container div with pixel divs, whose size and number is determined 
+// by the grid width and 4x3 aspect ratio. (Is there is a better way
+// to assign height than inline styling?) The container div background is black
+// and is revealed by reducing the opacity of the grey pixels in standard
+// and progressive modes.
 function createGrid(gridWidth) {
-    const pixelSize = (560 / gridWidth);
+    const pixelSize = (container.offsetWidth / gridWidth);
     for (let i = 0; i < (gridWidth * (gridWidth * 3 / 4)); i++) {
         const pixel = document.createElement('div');
+        pixel.dataset.passCount = 0;
         pixel.style.height = `${pixelSize}px`;
         pixel.style.width = `${pixelSize}px`;
         container.appendChild(pixel);
@@ -19,7 +26,6 @@ function createGrid(gridWidth) {
 function clearGrid() {
     const pixels = container.querySelectorAll('div');
     for (const div of pixels) {
-        div.classList.remove('active');
         container.removeChild(div);
     }
 }
@@ -28,6 +34,8 @@ let fillStyle = 'standard';
 const rainbowFill = [];
 let rainbowCounter = 0;
 
+// Creates an array of random hex color values to be iterated over when changing
+// pixel background color in rainbow mode. Runs once at page load.
 function createRainbow() {
     for (i = 0; i < 64; i++) {
         const x = Math.floor(Math.random() * 4096);
@@ -35,38 +43,46 @@ function createRainbow() {
     }
 }
 
+
+
+// When mouseover a child div (pixel) of container, check the fillStyle mode
+// and change opacity / background color / data-pass-count accordingly.
+//  When the grid is cleared, all divs are removed.
+
 container.addEventListener('mouseover', eventTarget => {
+
+    // Toggling the 'active' class to change opacity to 0
     if (fillStyle === 'standard') {
-        // eventTarget.target.style.opacity = '0';
         let classes = eventTarget.target.classList
         classes.add('active');
     }
+    // Changing the pixel background to one of the rainbowFill[] colors
     if (fillStyle === 'rainbow'){
         eventTarget.target.style.backgroundColor = `#${rainbowFill[rainbowCounter]}`;
         rainbowCounter >= 63 ? rainbowCounter = 0 : rainbowCounter++;
     }   
+    // Use data-pass-count attribute to track mouse passes, opacity is set
+    // in stylesheet
     if (fillStyle === 'progressive'){
-        console.log(`foo: ${eventTarget.target.style.width}`);
-        let x = Number(eventTarget.target.style.opacity)
-        
-        if (x > 0) eventTarget.target.style.opacity = String(x - 0.1);
+        const pass = Number(eventTarget.target.dataset.passCount);
+        if (pass < 10) eventTarget.target.dataset.passCount = String(pass + 1);
         }
     
 } 
 
 )
-container.addEventListener('mouseleave', eventTarget => {
-}, true)
 
-clear.addEventListener('click', () => {
+clearButton.addEventListener('click', () => {
     clearGrid();
-    clear.textContent = 'Clear'
+    clearButton.textContent = 'Clear'
     createGrid(sizeInput.value);
 })
-sizeInput.addEventListener('focus', () => clear.textContent = 'Change');
+
+// Clear button doubles as size change button
+sizeInput.addEventListener('focus', () => clearButton.textContent = 'Change');
 
 
-
+// Pass the fillStyle mode to determine mouseover behavior
 rightControls.addEventListener('click', eventTarget => {
     fillStyle = eventTarget.target.id;
 })
